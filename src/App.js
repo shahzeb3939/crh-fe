0d810f8804b212795b47393de40755fff3d7cb3a
Shahzeb Khan <shahzeb.khan@emumba.com>
2023-08-01 08:47:01 +0500

    Add react hook to call AWS Api Gateway endpoint.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import cv from './cv.png'
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://x6q0jgqi5k.execute-api.ap-south-1.amazonaws.com/crh-be');
+        const data = await response.json();
+        if (isMounted) {
+          setMessage(data.Message);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    if (!message) {
+      fetchData();
+    }
+
+    return () => {
+      isMounted = false;
+    };
+  }, [message]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>You are visitor number 3939.</p>      
+        <p>You are visitor number {message}.</p>      
         <img src={cv} alt="cv" className="full-width-image" />
       </header>
     </div>
